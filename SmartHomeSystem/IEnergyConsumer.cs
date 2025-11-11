b24e6be3006560c831e4cc1dcd252217aad255bb
dest1nycs <prokopenkokiril78@gmail.com>
2025-11-11 21:43:25 +0200

    lab4

---
+++ SmartHomeSystem/IEnergyConsumer.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartHomeSystem
+{
+    public interface IEnergyConsumer
+    {
+        string DeviceName { get; }
+        int PowerConsumption { get; }
+        double GetEnergyUsage(int hours);
+    }
+}
+
